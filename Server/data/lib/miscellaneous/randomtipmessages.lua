f03122a132d07714495e7dbf12534407b4ba0137
Gustavo Contreiras <guto.contreiras@gmail.com>
2019-11-10 12:27:17 -0300

    New tip messages

--- Server/data/lib/miscellaneous/randomtipmessages.lua
+++ Server/data/lib/miscellaneous/randomtipmessages.lua
@@ -1,10 +1,10 @@
 TIP_MESSAGES = {
     "Faith and Intelligence DOES NOT increase damage with spells and runes!",
-	"Raising your Level and Magic increases your damage with spells and runes!",
+	"Raising your Level and Magic is the only thing that increases your damage with spells and runes!",
 	"Dual Wielding increases your base attack speed to 150%!",
 	"Faith and Intelligence increases your power with wands and rods!",
 	"The Fighting Arena is under Rhyves, the entrance is on the East Gate!",
-	"Yasir is on the East Gate of Rhyves City!",
+	"Yasir buys creature items and is on the East Gate of Rhyves City!",
 	"You can buy magical rings near the depot of Rhyves!",
 	"The Guide on the Temple of Rhyves can reset your points!",
 	"Swords, axes, clubs and distance weapons have a chance to do critical hit and bleeding effect!",
